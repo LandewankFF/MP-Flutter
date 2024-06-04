ba16ca76716e0de624e9c1a5ae5519c95500284f
LandewankFF <landewankfahrezaf@gmail.com>
2024-06-04 16:52:03 +0700

    fix: Finish

---
+++ src/routes.php
@@ -0,0 +1,122 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'index.phtml', $args);
+    });
+
+    // method: GET
+    $app->get('/mahasiswa/pembayaran', function (Request $request, Response $response) {
+        // Disini dimasukan script SQL
+        $sql = "SELECT mahasiswa.nim, nama,
+                CASE
+                    WHEN jk='L' THEN 'Laki-Laki'
+                    WHEN jk='P' THEN 'Perempuan'
+                END as jenis_kelamin
+                , umur, IFNULL(semester,0) as semester, IFNULL(jumlah,0) as jumlah_spp
+                FROM mahasiswa LEFT JOIN spp_mahasiswa
+                ON mahasiswa.nim = spp_mahasiswa.nim WHERE mahasiswa.nim<>0";
+
+        $jk = $request->getQueryParam("jk");
+        $umur = $request->getQueryParam("umur");
+
+        if ($jk !== null) {
+            $sql .= " AND jk = :jk";
+        }
+        if ($umur !== null) {
+            $sql .= " AND umur = :umur";
+        }
+
+        $stmt = $this->db->prepare($sql);
+
+        if ($jk !== null) {
+            $stmt->bindParam(':jk', $jk);
+        }
+        if ($umur !== null) {
+            $stmt->bindParam(':umur', $umur);
+        }
+
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $response->withJson(["status" => "success", "data" => $result], 200);
+    });
+
+    // Method POST
+    $app->post('/mahasiswa/edit', function(Request $request, Response $response) {
+        $add_mahasiswa = $request->getParsedBody();
+        $nim = $add_mahasiswa['nim'];
+        $jk = $add_mahasiswa['jk'];
+        $nama = $add_mahasiswa['nama'];
+        $umur = $add_mahasiswa['umur'];
+
+        // Disini dimasukan script SQL
+        $sql = "INSERT INTO mahasiswa (nim, nama, jk, umur) VALUES (:nim, :nama, :jk, :umur)";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':nim', $nim);
+        $stmt->bindParam(':nama', $nama);
+        $stmt->bindParam(':jk', $jk);
+        $stmt->bindParam(':umur', $umur);
+
+        if ($stmt->execute()) {
+            return $response->withJson(["status" => "success", "data" => "Mahasiswa berhasil ditambahkan"], 200);
+        } else {
+            return $response->withJson(["status" => "error", "data" => "Gagal menambahkan mahasiswa"], 500);
+        }
+    });
+
+    // Method PUT
+    $app->put('/mahasiswa/edit', function(Request $request, Response $response){
+        $add_mahasiswa = $request->getParsedBody();
+        $nim = $add_mahasiswa['nim'];
+        $jk = $add_mahasiswa['jk'];
+        $nama = $add_mahasiswa['nama'];
+        $umur = $add_mahasiswa['umur'];
+
+
+        $textupdate= "";
+        if($nama<>null){
+            $textupdate = $textupdate.", nama='$nama'";
+        };
+        if($jk<>null){
+            $textupdate = $textupdate.", jk='$jk'";
+        };
+        if($umur<>null){
+            $textupdate = $textupdate.", umur=$umur";
+        };
+        //disini dimasukan script sql
+        $sql = "UPDATE mahasiswa SET nim=$nim".$textupdate." WHERE nim=$nim";
+
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $response->withJson(["status"=>"success", "data"=>"1"],200);
+    });
+
+    // Method Delete
+    $app->delete('/mahasiswa/edit', function(Request $request, Response $response){
+        $add_mahasiswa = $request->getParsedBody();
+        $nim = $add_mahasiswa['nim'];
+
+        //disini dimasukan script sql
+        $sql = "DELETE FROM mahasiswa WHERE nim=$nim";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $response->withJson(["status"=>"success", "data"=>"1"],200);
+    });
+
+
+};
+
+
